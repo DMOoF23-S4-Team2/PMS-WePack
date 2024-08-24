f2b36f9c13a4bfd459503bce0a97e8e128ddfffa
Adam Eleraky <adam_eleraky@hotmail.com>
2024-08-24 18:07:11 +0200

    Generic Repository implemented in Core

---
+++ src/PMS.Core/Repositories/Base/IRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using PMS.Core.Entities;
+
+namespace PMS.Core.Repositories.Base;
+
+ public interface IRepository<T> where T : class
+ {
+     Task<IReadOnlyList<T>> GetAllAsync();
+     Task<T> GetByIdAsync(int id);
+     Task<T> AddAsync(T entity);
+     Task UpdateAsync(T entity);
+     Task DeleteAsync(T entity);
+
+ }
---
+++ src/PMS.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,10 @@
+using System;
+using PMS.Core.Entities;
+using PMS.Core.Repositories.Base;
+
+namespace PMS.Core.Repositories;
+
+public interface IProductRepository : IRepository<Product>
+{
+
+}
