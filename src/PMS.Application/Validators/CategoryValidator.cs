e9452a47c7e6d967a1a1d0f7cdd906bcb6600e7e
trygvebechsgaard <trygvemb@gmail.com>
2024-09-24 15:18:59 +0700

    Refactor CategoryValidator.cs to use FluentValidation
    - Updated validation rules for category name, description, and bottom description
    - Removed explicit error messages, relying on default validation messages

--- src/PMS.Application/Validators/CategoryValidator.cs
+++ src/PMS.Application/Validators/CategoryValidator.cs
@@ -8,15 +8,15 @@ namespace PMS.Application.Validators
         public CategoryValidator()
         {
             RuleFor(x => x.Name)
-                .NotEmpty().WithMessage("Category name is required")
-                .MaximumLength(255).WithMessage("Category name must not exceed 255 characters");
+                .NotEmpty()
+                .MaximumLength(255);
 
             RuleFor(x => x.Description)
-                .NotEmpty().WithMessage("Category description is required")
-                .MaximumLength(5000).WithMessage("Category description must not exceed 5000 characters");
+                .NotEmpty()
+                .MaximumLength(5000);
 
             RuleFor(x => x.BottomDescription)
-                .MaximumLength(65535).WithMessage("Bottom description must not exceed 65535 characters");
+                .MaximumLength(65535);
 
             RuleForEach(x => x.Products)
                 .SetValidator(new ProductValidator());
