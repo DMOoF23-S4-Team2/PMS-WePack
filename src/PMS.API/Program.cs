ef787a023a0ad5279fc5eb61fa635a24c8d60ad6
trygvebechsgaard <trygvemb@gmail.com>
2024-11-15 12:50:10 +0700

    Remove unnecessary Docker configurations and update launch settings for Docker support

--- src/PMS.API/Program.cs
+++ src/PMS.API/Program.cs
@@ -15,34 +15,25 @@ using PMS.Infrastructure.Shopify;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Key Vault to configuration
-try
-{
+if (builder.Environment.IsProduction())
     builder.Configuration.AddAzureKeyVault(
         new Uri("https://keyvault-wepack.vault.azure.net/"),
         new DefaultAzureCredential(new DefaultAzureCredentialOptions
         {
             ManagedIdentityClientId = builder.Configuration["f19bc187-b11d-4d19-94b0-f4063acee199"]
         }));
-
-    // var keyVaultUri = builder.Configuration["KeyVaultUri"];
-    // builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUri ?? throw new ArgumentNullException("keyVaultUri not found")), new DefaultAzureCredential());
-}
-catch (Exception ex)
-{
-    var logger = LoggerFactory.Create(logging => logging.AddConsole()).CreateLogger<Program>();
-    logger.LogError(ex, "Error adding Azure Key Vault to configuration");
-}
+        
 // Configure CORS to make API requests from your host machine and the web service in the Docker network.
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowPmsWeb",
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:5002", "http://web:80", "https://containerapp-wepack-web-4.bluestone-4e633029.swedencentral.azurecontainerapps.io/")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
-});
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("AllowPmsWeb",
+//         policy =>
+//         {
+//             policy.WithOrigins("http://localhost:5002", "http://web:80", "https://containerapp-wepack-web-4.bluestone-4e633029.swedencentral.azurecontainerapps.io/")
+//                   .AllowAnyHeader()
+//                   .AllowAnyMethod();
+//         });
+// });
 
 // Register HttpClient
 builder.Services.AddHttpClient();
@@ -71,7 +62,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseCors("AllowPmsWeb");
+//app.UseCors("AllowPmsWeb");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
